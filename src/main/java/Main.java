092c05e35b4481fcc5574643c1c434a54dc9e443
Anton Goroshko <radjahair@gmail.com>
2024-03-07 13:11:48 +1000

    add Product

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,9 +30,7 @@ class Util {
 
         return Integer.parseInt(input);
     }
-}
 
-class Pluralizator {
     public static String pluralize(double number, String one, String few, String many) {
         int integerNum = (int)number;
         int remainder_10 = integerNum % 10;
@@ -55,63 +53,75 @@ class Pluralizator {
 }
 
 class Calculator {
+    Scanner scanner = new Scanner(System.in);
     int personNum;
-    ArrayList<String> basket = new ArrayList<>();
+    ArrayList<Product> products = new ArrayList<>();
     double total = 0.0;
 
     Calculator (int personNum) {
         this.personNum = personNum;
     }
 
-    public void addProduct(Scanner scanner) {
-        String productName = getProductName(scanner);
-        double productPrice = getProductPrice(scanner);
+    public void addProduct() {
+        Product product = new Product();
 
-        total += productPrice;
-        basket.add(productName);
-        checkNext(scanner);
+        products.add(product);
+        total += product.price;
+
+        checkNext();
     }
 
-    private void checkNext(Scanner scanner) {
-        System.out.println("Хотите ли Вы добавить еще товар?");
+    private void checkNext() {
+        System.out.println("Хотите ли Вы добавить еще товар? Если нет - введите 'завершить'.");
 
         String command = scanner.nextLine().trim().toLowerCase();
 
         if (!command.equals("завершить")) {
-            addProduct(scanner);
+            addProduct();
         } else {
-            showBasket();
+            showProducts();
             showResult();
         }
     }
 
-    private void showBasket() {
+    private void showProducts() {
         System.out.println("Добавленные товары:");
-        for (String product : basket) {
-            System.out.println(product);
+        for (Product product : products) {
+            System.out.println(product.name);
         }
     }
 
     private void showResult() {
         double result = total / personNum;
-        String pluralRuble = Pluralizator.pluralize(result, "рубль", "рубля", "рублей");
+        String pluralRuble = Util.pluralize(result, "рубль", "рубля", "рублей");
 
         System.out.printf("Каждый заплатит: %.2f %s%n", result, pluralRuble);
     }
+}
 
-    private static String getProductName(Scanner scanner) {
+class Product {
+    Scanner scanner = new Scanner(System.in);
+    String name;
+    double price;
+
+    Product () {
+        this.name = setName();
+        this.price = setPrice();
+    }
+
+    private String setName() {
         System.out.println("Введите название товара:");
         String productName = scanner.nextLine().trim();
 
         while (!isValidName(productName)) {
             System.out.println("Название слишком короткое, введите длинее:");
             productName = scanner.nextLine().trim();
-        };
+        }
 
         return productName;
     }
 
-    private static double getProductPrice(Scanner scanner) {
+    private double setPrice() {
         System.out.println("Введите стоимость товара в формате рубли.копейки(100.50):");
         String input = scanner.nextLine().trim();
 
@@ -119,18 +129,18 @@ class Calculator {
             if (!Util.isDoubleStr(input)) {
                 System.out.println("Цена должна быть указана в виде 100.50.\nПовторите ввод:");
             } else if (Double.parseDouble(input) == 0.0) {
-                System.out.println("Если товар ничего не стоит - то и делать его не стоит:)\nВведите цену:");
+                System.out.println("Стоимость товара не может быть нулевой.\nВведите цену:");
             } else {
                 System.out.println("Отрицательная стоимость бывает только у бочки нефти.\nВведите цену:");
             }
             input = scanner.nextLine().trim();
-        };
+        }
 
         return Double.parseDouble(input);
     }
 
-    private static boolean isValidName(String productName) {
-        return productName.length() > 1;
+    private static boolean isValidName(String str) {
+        return str.length() > 1;
     }
 
     private static boolean isValidPrice(String str) {
@@ -155,11 +165,11 @@ public class Main {
                 System.out.println("Отлично, Вы платите за все.");
                 return;
             }
-            System.out.println("Это некорректное значение, человек должно быть больше 1. Введите количество снова.");
+            System.out.println("Это некорректное значение, человек должно быть больше 1. Введите количество снова:");
 
             personsNum = Util.getIntegerInput(scanner);
         }
 
-        new Calculator(personsNum).addProduct(scanner);
+        new Calculator(personsNum).addProduct();
     }
 }
